6dc8bceb285939a9ca4a02a2d6bf925da27ab73f
dprekel <dprekel29@gmail.com>
2024-03-20 21:21:38 +0100

    Making progress with the minimal compiler interface, making sure that vtable offsets match between program and compiler library

--- driver/test/interface.h
+++ driver/test/interface.h
@@ -1,12 +1,142 @@
 
+namespace CIF {
+
+// generic interface for all components
+struct ICIF {
+    //ICIF(const ICIF &) = delete;
+    //ICIF &operator=(const ICIF &) = delete;
+    //ICIF(ICIF &&) = delete;
+    //ICIF *operator=(ICIF &&) = delete;
+    virtual void Release() = 0;
+    virtual void Retain() = 0;
+    virtual uint32_t GetRefCount() const = 0;
+    virtual uint64_t GetEnabledVersion() const = 0;
+    virtual uint64_t GetUnderlyingVersion() const;
+    virtual bool GetSupportedVersions(uint64_t intId, uint64_t &verMin, uint64_t &verMax) const;
+    //ICIF() = default;
+    virtual ~ICIF() = default;
+    virtual ICIF* CreateInterfaceImpl(uint64_t intId, uint64_t version);
+};
+
+
+
+struct CIFMain : public ICIF {
+    virtual uint64_t GetBinaryVersion() const = 0;
+//protected:
+    //CIFMain() = default;
+    virtual uint64_t FindIncompatibleImpl(uint64_t entryPointInterface, const void* handle) const = 0;
+    virtual bool FindSupportedVersionsImpl(uint64_t entryPointInterface, uint64_t interfaceToFind, uint64_t &verMin, uint64_t &verMax) const;
+};
+
+
+namespace Builtins {
+
+struct Buffer {
+    //virtual void SetAllocator(AllocatorT allocator, DeallocatorT deallocator, ReallocatorT reallocator);
+    //virtual void SetUnderlyingStorage(void* memory, size_t size, DeallocatorT deallocator);
+    virtual void SetUnderlyingStorage(const void* memory, size_t size);
+    virtual void* DetachAllocation();
+    virtual const void* GetMemoryRaw() const;
+    virtual void* GetMemoryRawWriteable();
+    virtual size_t GetSizeRaw() const;
+    virtual size_t GetCapacityRaw() const;
+    virtual bool Resize(size_t newSize);
+    virtual bool Reserve(size_t newCapacity);
+    virtual void Clear();
+    virtual void Deallocate();
+    virtual bool AlignUp(uint32_t alignment);
+    virtual bool PushBackRawBytes(const void* newData, size_t size);
+    virtual bool IsConst() const;
+};
+
+
+
+}
+
+/*
+template <uint64_t id, typename BaseClass = ICIF>
+struct NamedCIF : public BaseClass {
+    static constexpr uint64_t GetInterfaceId() {
+        return id;
+    };
+};
+*/
+
+
+}
+
+
 namespace IGC {
+
+
+struct Platform {
+    virtual uint64_t GetProductFamily() const;
+    virtual void SetProductFamily(uint64_t v);
+    virtual uint64_t GetPCHProductFamily() const;
+    virtual void SetPCHProductFamily(uint64_t v);
+    virtual uint64_t GetDisplayCoreFamily() const;
+    virtual void SetDisplayCoreFamily(uint64_t v);
+    virtual uint64_t GetRenderCoreFamily() const;
+    virtual void SetRenderCoreFamily(uint64_t v);
+    virtual uint64_t GetPlatformType() const;
+    virtual void SetPlatformType(uint64_t v);
+
+    virtual uint16_t GetDeviceID() const;
+    virtual void SetDeviceID(uint16_t v);
+    virtual uint16_t GetRevId() const;
+    virtual void SetRevId(uint16_t v);
+    virtual uint16_t GetDeviceID_PCH() const;
+    virtual void SetDeviceID_PCH(uint16_t v);
+    virtual uint16_t GetRevId_PCH() const;
+    virtual void SetRevId_PCH(uint16_t v);
+
+    virtual uint16_t GetGTType() const;
+    virtual void SetGTType(uint64_t v);
+};
+
+struct OclTranslationOutput {
+
+};
+
+struct FclOclTranslationCtx {
+    virtual OclTranslationOutput* TranslateImpl(uint64_t outVersion, CIF::Builtins::Buffer* src, CIF::Builtins::Buffer* options, CIF::Builtins::Buffer* internalOptions, CIF::Builtins::Buffer* tracingOptions, uint32_t tracingOptionsCount);
+};
+
+
+struct FclOclDeviceCtx : public CIF::ICIF {
+    struct Impl;
+    virtual Impl* GetImpl();
+    virtual const Impl* GetImpl() const;
+    virtual void SetOclApiVersion(uint32_t version);
+    virtual FclOclTranslationCtx* CreateTranslationCtxImpl(uint64_t ver, uint64_t inType, uint64_t outType);
+    virtual uint64_t GetPreferredIntermediateRepresentation();
+    virtual FclOclTranslationCtx* CreateTranslationCtxImpl(uint64_t ver, uint64_t inType, uint64_t outType, CIF::Builtins::Buffer* err);
+    virtual Platform* GetPlatformHandleImpl(uint64_t ver);
+};
+
+
+
+
+}
+
+
+
+
+/*
+namespace IGC {
+
+struct NamedCIF : public ICIF {
+    static constexpr uint64_t GetInterfaceId() {
+        return 95846467711642693;
+    };
+};
 
 //#define CIF_DECLARE_INTERFACE(NAME, ID)
 template <uint64_t version = CIF::InvalidVersion>
 struct FclOclDeviceCtx;
 
 template <>
-struct FclOclDeviceCtx<CIF::BaseVersion> : public CIF::NamedCIF<CIF::InterfaceIdCoder::Enc("FCL_OCL_DEVC")> {
+struct FclOclDeviceCtx<CIF::BaseVersion> : public NamedCIF { //<CIF::InterfaceIdCoder::Enc("FCL_OCL_DEVC")> {
     struct Impl;
     virtual Impl * GetImpl(){ 
         return pImpl;
@@ -21,134 +151,13 @@ struct FclOclDeviceCtx<CIF::BaseVersion> : public CIF::NamedCIF<CIF::InterfaceId
     FclOclDeviceCtx(ArgsT &&... args);
     FclOclDeviceCtx(Impl* pImpl);
 };
-using FclOclDeviceCtxBase = FclOclDeviceCtx<CIF::BaseVersion>;
-using FclOclDeviceCtxTagOCL = FclOclDeviceCtx<FclOclDeviceCtx<CIF::TraitsSpecialVersion>::GetLatestSupportedVersion()>; 
+//using FclOclDeviceCtxBase = FclOclDeviceCtx<CIF::BaseVersion>;
+//using FclOclDeviceCtxTagOCL = FclOclDeviceCtx<FclOclDeviceCtx<CIF::TraitsSpecialVersion>::GetLatestSupportedVersion()>; 
 
 
 //CIF_DECLARE_INTERFACE(FclOclDeviceCtx, "FCL_OCL_DEVC");
 
-}
-
-
-namespace CIF {
-
-
-// generic interface for all components
-struct ICIF {
-  ICIF(const ICIF &) = delete;
-  ICIF &operator=(const ICIF &) = delete;
-  ICIF(ICIF &&) = delete;
-  ICIF *operator=(ICIF &&) = delete;
-
-  virtual void Release() = 0;
-  virtual void Retain() = 0;
-  virtual uint32_t GetRefCount() const = 0;
-
-  virtual uint64_t GetEnabledVersion() const = 0;
-  virtual uint64_t GetUnderlyingVersion() const {
-      return GetEnabledVersion(); // by default : redirect to enabled version
-  }
-  virtual bool GetSupportedVersions(uint64_t intId, uint64_t &verMin,
-                                    uint64_t &verMax) const {
-      return false; // by default : no sub-interface are supported
-  }
-
-  // get specific version of interface
-  template <typename InterfaceT, uint64_t version>
-  RAII::UPtr_t<InterfaceT> CreateInterface() {
-    static_assert((version >= InterfaceT::GetBackwardsCompatibilityVersion()) &&
-                      (version <= InterfaceT::GetVersion()),
-                  "Invalid version requested");
-    return RAII::UPtr(reinterpret_cast<InterfaceT *>(
-        CreateInterfaceImpl(InterfaceT::GetInterfaceId(), version)));
-  }
-
-  // get latest version of interface that matches interface class
-  template <typename InterfaceT>
-  RAII::UPtr_t<InterfaceT> CreateInterface() {
-    uint64_t minVerSupported = 0;
-    uint64_t maxVerSupported = 0;
-    if (false == GetSupportedVersions(InterfaceT::GetInterfaceId(), minVerSupported, maxVerSupported)) {
-      // interface not supported
-      return RAII::UPtr<InterfaceT>(nullptr);
-    }
-
-    if ((InterfaceT::GetVersion() < minVerSupported) || (InterfaceT::GetVersion() > maxVerSupported)) {
-      // interface version not supported
-      return RAII::UPtr<InterfaceT>(nullptr);
-    }
-
-    // get latest compatible
-    uint64_t chosenVersion = std::min(maxVerSupported, InterfaceT::GetVersion());
-
-    return RAII::UPtr(reinterpret_cast<InterfaceT*>(CreateInterfaceImpl(InterfaceT::GetInterfaceId(), chosenVersion)));
-  }
-
-protected:
-  ICIF() = default;
-  virtual ~ICIF() = default;
-
-  virtual ICIF *CreateInterfaceImpl(uint64_t intId, uint64_t version) {
-      return nullptr; // by default : no sub-interface are supported
-  }
-};
-
-
-
-
-
-
-
-
-struct CIFMain : public ICIF {
-  virtual uint64_t GetBinaryVersion() const = 0;
-
-  template <template <uint64_t> class InterfaceEntryPoint> bool IsCompatible(const std::vector<uint64_t> *interfacesToIgnore = nullptr) {
-      return FindIncompatible<InterfaceEntryPoint>(interfacesToIgnore) == InvalidInterface;
-  }
-
-  template <template <uint64_t> class InterfaceEntryPoint>
-  uint64_t FindIncompatible(const std::vector<uint64_t> *interfacesToIgnore = nullptr);
-  /*
-      CompatibilityEncoder encoder;
-      auto encoded = encoder.Encode<InterfaceEntryPoint>(interfacesToIgnore);
-      return FindIncompatibleImpl(InterfaceEntryPoint<BaseVersion>::GetInterfaceId(), encoded);
-  }
-  */
-
-  template <template <uint64_t> class InterfaceEntryPoint>
-  bool FindSupportedVersions(uint64_t subIntId, uint64_t &verMin, uint64_t &verMax) const {
-      return FindSupportedVersionsImpl(InterfaceEntryPoint<BaseVersion>::GetInterfaceId(), subIntId, verMin, verMax);
-  }
-
-  // get latest version of builtin that matches interface class
-  template <typename BuiltinT>
-  RAII::UPtr_t<BuiltinT> CreateBuiltin() {
-      return CreateInterface<BuiltinT>();
-  }
-
-protected:
-    CIFMain() = default;
-
-    virtual uint64_t FindIncompatibleImpl(uint64_t entryPointInterface, const void* handle) const = 0;
-    virtual bool FindSupportedVersionsImpl(uint64_t entryPointInterface, uint64_t interfaceToFind, uint64_t &verMin, uint64_t &verMax) const {
-        Abort(); // make pure-virtual once all mocks addapt to new interface
-        return false;
-    }
-};
-
-template <uint64_t id, typename BaseClass = ICIF>
-struct NamedCIF : public BaseClass {
-    static constexpr uint64_t GetInterfaceId() {
-        return id;
-    };
-};
-
-
-
 
 }
-
-
-
+*/
 
--- driver/test/main.cpp
+++ driver/test/main.cpp
@@ -5,10 +5,38 @@
 #include <limits>
 #include <dlfcn.h>
 #include <stdint.h>
-#include "deps.h"
+
 #include "interface.h"
 
 #define COMPILER_LOAD_FAILED -1
+
+int loadCompiler(const char* libName);
+IGC::FclOclDeviceCtx* CreateInterface(CIF::CIFMain* cifMain);
+
+IGC::FclOclDeviceCtx* CreateInterface(CIF::CIFMain* cifMain) {
+    uint64_t chosenVersion;
+    uint64_t minVerSupported = 0;
+    uint64_t maxVerSupported = 0;
+    uint64_t interfaceID = 95846467711642693;
+    uint64_t version = 5;
+    bool isSupported;
+
+    isSupported = cifMain->GetSupportedVersions(interfaceID, minVerSupported, maxVerSupported);
+    printf("maxVerSupported: %lu\n", maxVerSupported);
+    printf("minVerSupported: %lu\n", minVerSupported);
+    if (isSupported == false) {
+        return nullptr;
+    }
+    if ((version < minVerSupported) || (version > maxVerSupported)) {
+        return nullptr;
+    }
+    printf("Versions are ok\n");
+    chosenVersion = std::min(maxVerSupported, version);
+    
+    CIF::ICIF* deviceCtx = cifMain->CreateInterfaceImpl(95846467711642693, 5);
+    return static_cast<IGC::FclOclDeviceCtx*>(deviceCtx);
+}
+
 
 int loadCompiler(const char* libName) {
     void* handle;
@@ -29,9 +57,27 @@ int loadCompiler(const char* libName) {
     cifMain = CreateCIFMainFunc();
     if (cifMain != nullptr) {
         printf("Could create main entry point\n");
-        return COMPILER_LOAD_FAILED;
+        //return COMPILER_LOAD_FAILED;
     }
-    auto newDeviceCtx = cifMain->CreateInterface<IGC::FclOclDeviceCtxTagOCL>();
+    IGC::FclOclDeviceCtx* newDeviceCtx = CreateInterface(cifMain);
+
+    if (newDeviceCtx == nullptr) {
+        printf("No Device Context!\n");
+    }
+    uint32_t openCLVersion = 30;
+    newDeviceCtx->SetOclApiVersion(openCLVersion * 10);
+    // maybe handle unsupported OpenCL version
+    if (newDeviceCtx->GetUnderlyingVersion() > 4U) {
+        /*
+        auto igcPlatform = deviceCtx->GetPlatformHandle();
+        if (nullptr == igcPlatform) {
+            return -1;
+        }
+        */
+        printf("ok\n");
+        //IGC::PlatformHelper::PopulateInterfaceWith(*igcPlatform, *hwInfo->platform);
+        //PopulateInterfaceWith(*igcPlatform, *hwInfo->platform);
+    }
     return 0;
 }
 
@@ -40,3 +86,6 @@ int main() {
     int err = loadCompiler(fclName);
     return 0;
 }
+
+
+
