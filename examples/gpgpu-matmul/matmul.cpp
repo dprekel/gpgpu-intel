8bf08f4239938880f55008b418d5d202250b0af8
dprekel <dprekel29@gmail.com>
2024-04-27 21:43:09 +0200

    Add object release to matmul example; update tasks

--- examples/gpgpu-matmul/matmul.cpp
+++ examples/gpgpu-matmul/matmul.cpp
@@ -10,14 +10,11 @@
 #define TILE_GROUP_N    1
 
 int main() {
-    int err;
-    const char* filename;
-
     GPU* gpuInfo = (GPU*)malloc(sizeof(GPU));
-    err = CreateDevice(gpuInfo);
+    int err = CreateDevice(gpuInfo);
     GetInfo(gpuInfo);
 
-    filename = "matmul.cl";
+    const char* filename = "matmul.cl";
     std::string build_options = "-DTILE_SIZE_M=" + std::to_string(TILE_SIZE_M)
                               + " -DTILE_GROUP_M=" + std::to_string(TILE_GROUP_M)
                               + " -DTILE_SIZE_N=" + std::to_string(TILE_SIZE_N)
@@ -28,7 +25,15 @@ int main() {
     printf("Context creation: %d\n", err);
     //void* ptrToBuffer = CreateBuffer(gpuInfo, 4096);
     //printf("Buffer ptr: %p\n", ptrToBuffer);
-    err = EnqueueNDRangeKernel(gpuInfo);
 
+    size_t size = 3968;
+    // number of work items per work group dimension
+    const size_t local[2] = {TILE_GROUP_M, TILE_GROUP_N};
+    // total number of work items in each dimension
+    const size_t global[2] = {size/TILE_SIZE_M, size/TILE_SIZE_N};
+    err = EnqueueNDRangeKernel(gpuInfo, 2, NULL, global, local);
+    printf("err: %d\n", err);
+
+    err = ReleaseObjects(gpuInfo);
     return 0;
 }
