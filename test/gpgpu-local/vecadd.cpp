5a31b37b7133eb1947002056071cff21d9da200e
dprekel <dprekel29@gmail.com>
2024-08-14 16:15:32 +0200

    Fix kernel argument size for __local qualifier

--- test/gpgpu-local/vecadd.cpp
+++ test/gpgpu-local/vecadd.cpp
@@ -44,14 +44,15 @@ int main() {
         vecBMem[i] = 1.0;
     }
 
+    size_t local_size = 256;
     err = SetKernelArg(kernel, 0, sizeof(vector_A), static_cast<void*>(vector_A));
     err = SetKernelArg(kernel, 1, sizeof(vector_B), static_cast<void*>(vector_B));
     err = SetKernelArg(kernel, 2, sizeof(vector_C), static_cast<void*>(vector_C));
-    err = SetKernelArg(kernel, 3, vector_size, nullptr);
-    err = SetKernelArg(kernel, 4, vector_size, nullptr);
+    err = SetKernelArg(kernel, 3, local_size * sizeof(float), nullptr);
+    err = SetKernelArg(kernel, 4, local_size * sizeof(float), nullptr);
     printf("[DEBUG] SetKernelArg: %d\n", err);
     // number of work items per work group dimension
-    const size_t local[3] = {256, 1, 1};
+    const size_t local[3] = {local_size, 1, 1};
     // total number of work items in each dimension
     const size_t global[3] = {15745024, 1, 1};
 
